c1dc43aa11d4efaaa9c3f7843d56f00f9e92a9c9
Justin Onyango <ojustin446@gmail.com>
2024-04-27 21:33:59 +0300

    Completed lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe () {
+    function Blur () {
+        console.log("Hey! Eyes on me!");
+    }
+    function Focus() {
+        console.log("Good!");
+    }
+    
+    return (
+        <button onFocus={Focus} onBlur={Blur}>
+            Eyes on me
+        </button>
+    );
+}
+
+export default EyesOnMe;
